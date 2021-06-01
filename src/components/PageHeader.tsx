6eda87da822fa8b03e02e6ee2438471405195550
Teemu Koivisto <tkoivisto456@gmail.com>
2021-06-01 11:44:59 +0300

    Update links to PageHeader

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -10,7 +10,10 @@ export function PageHeader(props: IProps) {
   return (
     <Container className={className}>
       <header>
-        <h1>Track changes example</h1>
+        <h1><a href="https://teemukoivisto.github.io/prosemirror-track-changes-example/">
+          Track changes example</a></h1>
+        <p>With prosemirror-changeset</p>
+        <p><a href="https://github.com/TeemuKoivisto/prosemirror-track-changes-example">Github repo</a></p>
       </header>
     </Container>
   )
